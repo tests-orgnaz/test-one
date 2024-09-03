32086b63d25e5aa6afc42a56d9e8d7467f991e28
jayspatel <jayspatel2153@gmail.com>
2024-09-03 12:14:42 +0530

    change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
   app.use(express.json());
   
   app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World...');
   });
 
   app.listen(port, () => {
